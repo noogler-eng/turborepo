e5d0d1bf25ef2c60c6615f67aeeaa5048e302d63
noogler-eng <sharadpoddar10102003@gmail.com>
2025-03-23 16:42:27 +0530

    ec2 conn

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,5 @@
 import prisma from "@repo/db/client";
 
 export default function Home() {
-  return <div>this is home page for Web adding something neww ...... soemthing more new</div>;
+  return <div>this is home page for Web adding something neww ........ soemthing more new</div>;
 }
